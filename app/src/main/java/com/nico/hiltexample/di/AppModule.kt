12dd21e6b37dedb665090f27af8b4e97448888e7
Nico <johnnicomolon@yahoo.com>
2021-10-06 04:26:43 -0400

    Initial commit

---
+++ app/src/main/java/com/nico/hiltexample/di/AppModule.kt
@@ -0,0 +1,27 @@
+package com.nico.hiltexample.di
+
+import android.content.Context
+import com.nico.hiltexample.BaseApplication
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideApplication(@ApplicationContext app: Context) : BaseApplication {
+        return app as BaseApplication
+    }
+
+    @Singleton
+    @Provides
+    fun randomString(): String{
+        return "This is a random string dasdsadasd"
+    }
+}
